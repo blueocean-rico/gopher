101c5a00e50080e23c4d2a3f4f2d619f68fb3f23
Shannon Largman <shannon.largman@gmail.com>
2022-06-26 23:52:37 -0700

    add getShoppingListItems to dal

--- server/shopping-lists/shopping-lists.dal.ts
+++ server/shopping-lists/shopping-lists.dal.ts
@@ -1,6 +1,27 @@
 import sql from '@/db/index'
 
-export function getShoppingListItems(listIds) {
+export async function getShoppingListItems(listIds) {
+  const result = await sql`
+    SELECT json_build_object(
+      list_id, json_agg(selected_list_items)
+    ) AS lists FROM (
+      SELECT
+        li.id AS list_item_id,
+        li.list_id AS list_id,
+        li.name AS name,
+        li.price AS price,
+        json_agg(u.id) AS users
+      FROM
+        list_items li
+          LEFT JOIN list_items_users li_u ON li.id = li_u.list_item_id
+          LEFT JOIN users u ON u.id = li_u.id
+      WHERE li.active = true AND li.list_id IN ${sql(listIds)}
+      GROUP BY li.id
+    ) AS selected_list_items
+    GROUP BY list_id;
+  `;
+
+  return result[0].lists;
 }
 
 export function getShoppingListEvents(listIds) {
@@ -35,7 +56,7 @@ export async function addShoppingListItem(item) {
       inserted_item.name AS name,
       inserted_item.price AS price,
       users_array.users AS users,
-      inserted_event.date AS date
+      inserted_event.event_date AS date
     FROM inserted_item, inserted_event, (
       SELECT json_agg(
         json_build_object(
