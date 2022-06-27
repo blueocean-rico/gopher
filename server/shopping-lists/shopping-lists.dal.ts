1afa363361472c729b4baba6fd0e34ace764e572
Shannon Largman <shannon.largman@gmail.com>
2022-06-27 01:31:39 -0700

    simplify getShoppingListItems
    Only handles the case for a single list, but this is likely the only
    case that will be needed.

--- server/shopping-lists/shopping-lists.dal.ts
+++ server/shopping-lists/shopping-lists.dal.ts
@@ -1,30 +1,54 @@
 import sql from '@/db/index'
 
-export async function getShoppingListItems(listIds) {
-  const result = await sql`
-    SELECT json_build_object(
-      list_id, json_agg(selected_list_items)
-    ) AS lists FROM (
-      SELECT
-        li.id AS list_item_id,
-        li.list_id AS list_id,
-        li.name AS name,
-        li.price AS price,
-        json_agg(u.id) AS users
-      FROM
-        list_items li
-          LEFT JOIN list_items_users li_u ON li.id = li_u.list_item_id
-          LEFT JOIN users u ON u.id = li_u.id
-      WHERE li.active = true AND li.list_id IN ${sql(listIds)}
-      GROUP BY li.id
-    ) AS selected_list_items
-    GROUP BY list_id;
+export function getShoppingListItems(listId) {
+  return sql`
+    SELECT
+      li.id AS list_item_id,
+      li.list_id AS list_id,
+      li.name AS name,
+      li.price AS price,
+      json_agg(json_build_object('id', u.id)) AS users
+    FROM
+      list_items li
+        LEFT JOIN list_items_users li_u ON li.id = li_u.list_item_id
+        LEFT JOIN users u ON u.id = li_u.id
+    WHERE li.active = true AND li_u.active = true AND li.list_id = ${listId}
+    GROUP BY li.id;
   `;
-
-  return result[0].lists;
 }
 
 export function getShoppingListEvents(listIds) {
+  const result = await sql`
+    SELECT json_agg(selected_events) AS events FROM (
+      SELECT es.start FROM (
+        SELECT e.event_type, e.user_id, e.end_id, e.event_date, json_build_object(
+          'list_item_id', li.id,
+          'list_id', li.list_id,
+          'name', li.name,
+          'price', li.price,
+          'users', json_agg(u1.id)
+        ) AS start
+        FROM
+          list_events e
+            LEFT JOIN users u ON e.user_id = u.id
+            LEFT JOIN list_items li1 ON e.start_id = li1.id
+            LEFT JOIN list_items_users li_u1 ON li1.id = li_u1.list_item_id
+            LEFT JOIN users u1 ON u1.id = li_u1.user_id
+        WHERE li.list_id IN (1,2)
+        --- WHERE li.list_id IN ${sql(listIds)}
+        GROUP BY li1.id
+      ) AS es 
+        LEFT JOIN list_items li2 ON es.end_id = li2.id
+        LEFT JOIN list_items_users li_u2 ON li2.id = li_u2.list_item_id
+        LEFT JOIN users u2 ON u2.id = li_u2.user_id
+
+  AS selected_events
+
+    GROUP BY list_id;
+  `;
+
+  return result[0].events;
+}
 }
 
 export function getShoppingListMembers(listIds) {
