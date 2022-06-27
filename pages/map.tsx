7e115119536112cc82e80d522b0e8b3efe04ad64
Sully Clark <clarkjs237@gmail.com>
2022-06-27 12:05:08 -0700

    successfully display map

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,9 +1,42 @@
 import type { NextPage } from "next";
 
+// const Map: NextPage = () => {
+//   return (
+//     <div>{process.env.NEXT_PUBLIC_GOOGLE_MAPS}</div>
+//   )
+// }
+
+// export default Map;
+
+
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
 const Map: NextPage = () => {
   return (
-    <div>{process.env.NEXT_PUBLIC_GOOGLE_MAPS}</div>
+    <LoadScript
+      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
-export default Map;
+export default React.memo(Map)
