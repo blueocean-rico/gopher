095f97e9ce071e6c881dbc7776f743b7f9dc05a6
Shannon Largman <shannon.largman@gmail.com>
2022-06-26 23:02:03 -0700

    finish addShoppingListItem for db

--- server/shopping-lists/shopping-lists.dal.ts
+++ server/shopping-lists/shopping-lists.dal.ts
@@ -9,65 +9,43 @@ export function getShoppingListEvents(listIds) {
 export function getShoppingListMembers(listIds) {
 }
 
-insert into users(name) values ('me');
-
-export 
-
-
-item.users.map(user => `(inserted_item.id, ${user.id})`).join(', ')
-let item = {list_id: 1, name:'carrot', price:100, users: [{id:1, name: 'me'}]}
-
-test(item).strings
-test(item).args[3].first[0].list_item_id;
-test(item).then(x=> console.log(x));
-
-test = function addShoppingListItem(item) {
-  //insert into users as well
-  return sql`
+export async function addShoppingListItem(item) {
+  const result = await sql`
     WITH inserted_item AS (
-      INSERT INTO list_items (
-        list_id,
-        name,
-        price
-      ) VALUES (
-        ${item.list_id},
-        ${item.name},
-        ${item.price}
-      ) RETURNING *
+      INSERT INTO list_items (list_id, name, price)
+      VALUES (${item.list_id}, ${item.name}, ${item.price})
+      RETURNING *
     ),
     inserted_users AS (
-      INSERT INTO list_items_users
-      ${sql(item.users.map((user) => (
-        {
-          list_item_id: sql`inserted_itemid`,
-          user_id: user.id
-        }
-      )))}
+      INSERT INTO list_items_users (list_item_id, user_id)
+      SELECT * FROM (
+        (SELECT id FROM inserted_item) AS alias1 CROSS JOIN unnest(
+          ${item.users.map((user) => user.id)}::integer[]
+        )
+      ) AS alias2
       RETURNING *
     ),
     inserted_event AS (
-      INSERT INTO list_events (
-        event_type,
-        end_id
-      ) SELECT 'add', id FROM inserted_item
+      INSERT INTO list_events (event_type, end_id)
+      SELECT 'add', id FROM inserted_item
       RETURNING *
-    ),
-    SELECT json_build_object(
-      'id', inserted_item.id,
-      'name', inserted_item.name,
-      'price', inserted_item.price,
-      'users', users,
-      'type', inserted_event.event_type
-    ) AS result
+    )
+    SELECT
+      inserted_item.id AS id,
+      inserted_item.name AS name,
+      inserted_item.price AS price,
+      users_array.users AS users,
+      inserted_event.date AS date
     FROM inserted_item, inserted_event, (
       SELECT json_agg(
         json_build_object(
-          'id', id,
-          'name', name
-        ) users
-      ) FROM inserted_users
-    );
+          'id', id
+        )
+      ) AS users FROM inserted_users
+    ) AS users_array;
   `;
+
+  return result[0];
 }
 
 export function deleteShoppingListItem(item) {
