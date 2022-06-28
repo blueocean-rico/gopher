0f0e028f767d091caa52a81d0777f67736c944de
Sully Clark <clarkjs237@gmail.com>
2022-06-27 18:47:18 -0700

    marker staying on point

--- pages/map.tsx
+++ pages/map.tsx
@@ -10,11 +10,14 @@ import type { NextPage } from "next";
 
 
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, useJsApiLoader, MarkerF } from '@react-google-maps/api';
+import styled from 'styled-components';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '750px',
+  height: '500px'
+  // width: '100%',
+  // height: '100%'
 };
 
 const center = {
@@ -22,20 +25,47 @@ const center = {
   lng: -38.523
 };
 
+const MapWrapper = styled.div`
+  position: absolute;
+  max-height: 500px;
+  min-height: 500px;
+  max-width: 750px;
+  min-width: 750px;
+`;
+
+
+const google_maps_api_key: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS ?? '';
+
 const Map: NextPage = () => {
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: google_maps_api_key,
+  })
+
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS}
-    >
-      <GoogleMap
+    <MapWrapper>
+        <GoogleMap
         mapContainerStyle={containerStyle}
+        // mapContainerStyle={{width: '100%', height: '100%'}}
         center={center}
         zoom={10}
-      >
+        options={{
+          zoomControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false
+        }}
+        >
         { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+
+        <MarkerF position={center} />
       </GoogleMap>
-    </LoadScript>
+    </MapWrapper>
+
   )
 }
 
