41ef6bb4d0b3b15fe52ab4bd9c3849862d7845b2
Sully Clark <clarkjs237@gmail.com>
2022-06-27 21:03:59 -0700

    got maps added using locations and marker

--- pages/map.tsx
+++ pages/map.tsx
@@ -6,75 +6,10 @@
 // //   )
 // // }
 
-// // export default Map;
-
-
-// import React from 'react'
-// import { GoogleMap, LoadScript, useJsApiLoader, MarkerF } from '@react-google-maps/api';
-// import styled from 'styled-components';
-
-// const containerStyle = {
-//   width: '750px',
-//   height: '500px'
-//   // width: '100%',
-//   // height: '100%'
-// };
-
-// const center = {
-//   lat: -3.745,
-//   lng: -38.523
-// };
-
-// const MapWrapper = styled.div`
-//   position: absolute;
-//   max-height: 500px;
-//   min-height: 500px;
-//   max-width: 750px;
-//   min-width: 750px;
-// `;
-
-
-// const google_maps_api_key: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS ?? '';
-
-// const Map: NextPage = () => {
-
-  // const { isLoaded } = useJsApiLoader({
-  //   googleMapsApiKey: google_maps_api_key,
-  // })
-
-  // if (!isLoaded) {
-  //   return <div>Loading...</div>
-  // }
-
-//   return (
-//     <MapWrapper>
-//         <GoogleMap
-//         mapContainerStyle={containerStyle}
-//         // mapContainerStyle={{width: '100%', height: '100%'}}
-//         center={center}
-//         zoom={10}
-        // options={{
-        //   zoomControl: false,
-        //   streetViewControl: false,
-        //   mapTypeControl: false,
-        //   fullscreenControl: false
-        // }}
-//         >
-//         { /* Child components, such as markers, info windows, etc. */ }
-
-//         <MarkerF position={center} />
-//       </GoogleMap>
-//     </MapWrapper>
-
-//   )
-// }
-
-// export default React.memo(Map)
-
-
 import React, { useState } from "react";
 import { GoogleMap, LoadScript, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import PlaceSearch from "./api/map";
+import { constants } from "buffer";
 
 const containerStyle = {
   width: '750px',
@@ -92,10 +27,10 @@ const center = {
 const google_maps_api_key: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS ?? '';
 
 
-function HomePage() {
-  const [places, setPlaces] = useState(PlaceSearch({ center }));
+function HomePage({ stores }) {
+  // const [places, setPlaces] = useState(PlaceSearch({ center }));
 
-  // console.log(places);
+  console.log(stores);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: google_maps_api_key,
@@ -106,21 +41,57 @@ function HomePage() {
   }
 
   return (
-    // <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS} places>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={12}
-        options={{
-          zoomControl: false,
-          streetViewControl: false,
-          mapTypeControl: false,
-          fullscreenControl: false
-        }}
-      >
-        <MarkerF position={center} />
-      </GoogleMap>
-    // </LoadScript>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={13}
+      options={{
+        zoomControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        fullscreenControl: false
+      }}
+    >
+      <MarkerF position={center} icon={'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'} />
+      {stores.map((store, index) => (
+        <MarkerF position={store.geometry.location} key={index} />
+      ))}
+    </GoogleMap>
   );
 }
+
+
+export async function getStaticProps() {
+
+  const center = {
+    lat: 47.6205,
+    lng: -122.349358,
+  };
+
+  const config = {
+    method: "GET",
+    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${center.lat}%2C${center.lng}&radius=1800&type=supermarket&keyword=whole&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS}`,
+  };
+
+
+  const res = await fetch(config.url, {
+    method: 'GET',
+    // mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await res.json();
+
+  const stores = data.results;
+
+  return {
+    props: {
+      stores
+    }
+  };
+}
+
+
 export default HomePage;
