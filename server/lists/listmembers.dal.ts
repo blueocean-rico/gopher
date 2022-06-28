4d4066fb9ed728afdf1cd703d64a400973d8c593
Shannon Largman <shannon.largman@gmail.com>
2022-06-28 15:28:29 -0700

    add backend for list members

---
+++ server/lists/listmembers.dal.ts
@@ -0,0 +1,32 @@
+import sql from '@/db/index';
+import { User } from '@/types/index';
+
+export async function getShoppingListMembers(listId: number) {
+  const result = (await sql`
+    SELECT u.*
+    FROM lists_users l_u INNER JOIN users u ON l_u.user_id = u.id
+    WHERE l_u.list_id = ${listId};
+  `) as any as User[];
+
+  return { list_id: listId, users: result };
+}
+
+export function addShoppingListMembers(listId: number, users: User[]) {
+  return sql`
+    INSERT INTO lists_users ${sql(
+      users.map((user) => ({
+        list_id: listId,
+        user_id: user.id,
+      })),
+      'user_id',
+      'list_id'
+    )}
+  `;
+}
+
+export function deleteShoppingListMembers(listId: number, users: User[]) {
+  return sql`
+    DELETE FROM lists_users WHERE list_id = ${listId} AND
+      user_id IN ${sql(users.map((user) => user.id))};
+  `;
+}
--- server/server.test.ts
+++ server/server.test.ts
@@ -1,9 +1,22 @@
 import postgres from 'postgres';
 import * as Types from '@/types/index';
 import { addUser } from '@/server/users/index';
-import { addShoppingList, deleteShoppingList, modifyShoppingList, getShoppingLists } from '@/server/lists/lists.dal';
+import {
+  addShoppingList,
+  deleteShoppingList,
+  modifyShoppingList,
+  getShoppingLists,
+  addShoppingListMembers,
+  getShoppingListMembers,
+  deleteShoppingListMembers,
+} from '@/server/lists/index';
 
 const sql = postgres('postgres://postgres:postgres@localhost:5433/gopher');
+
+afterEach(async () => {
+  await sql`DELETE FROM users`;
+  await sql`DELETE FROM lists`;
+});
 
 afterAll(async () => {
   await sql.end();
@@ -21,9 +34,7 @@ describe('server', () => {
       await addUser(user);
 
       const results = await sql`SELECT * FROM users`;
-      await sql`DELETE FROM users`;
-
-      expect(results.length).toEqual(1);
+      expect(results).toHaveLength(1);
 
       const { nickname, email, picture } = results[0];
       expect(nickname).toEqual(user.nickname);
@@ -38,9 +49,7 @@ describe('server', () => {
       await addShoppingList(list);
 
       const results = await sql`SELECT * FROM lists`;
-      await sql`DELETE FROM lists`;
-
-      expect(results.length).toEqual(1);
+      expect(results).toHaveLength(1);
 
       const { name } = results[0];
       expect(name).toEqual(list.name);
@@ -55,7 +64,7 @@ describe('server', () => {
       await deleteShoppingList(listId);
       results = await sql`SELECT * FROM lists`;
 
-      expect(results.length).toEqual(0);
+      expect(results).toHaveLength(0);
     });
 
     it('should modify a shopping list', async () => {
@@ -70,17 +79,99 @@ describe('server', () => {
       results = await sql`SELECT * FROM lists WHERE id = ${addedList.id}`;
       const modifiedList = results[0];
       expect(modifiedList.name).toBe('newname');
-
-      await sql`DELETE FROM lists`;
     });
   });
 
   describe('listmembers', () => {
+    it('should add a shopping list member', async () => {
+      const list = { name: 'testlist' };
+      await addShoppingList(list);
+
+      let results = await sql`SELECT * FROM lists`;
+      const listId = results[0].id;
+
+      const user = {
+        nickname: 'test',
+        email: 'test@email.com',
+        picture: 'https://picture.com',
+      };
+      await addUser(user);
+
+      results = await sql`SELECT * FROM users`;
+      const addedUser = results[0] as Types.User;
+
+      await addShoppingListMembers(listId, [addedUser]);
+      results = await sql`SELECT * FROM lists_users`;
+
+      expect(results[0].list_id).toEqual(listId);
+      expect(results[0].list_id).toEqual(listId);
+    });
+
+    it('should get shopping list members', async () => {
+      const list = { name: 'testlist' };
+      await addShoppingList(list);
+
+      let results = await sql`SELECT * FROM lists`;
+      const listId = results[0].id;
+
+      const user1 = {
+        nickname: 'test1',
+        email: 'test1@email.com',
+        picture: 'https://picture1.com',
+      };
+      const user2 = {
+        nickname: 'test2',
+        email: 'test2@email.com',
+        picture: 'https://picture2.com',
+      };
+
+      await addUser(user1);
+      await addUser(user2);
+
+      results = await sql`SELECT * FROM users`;
+      const [addedUser1, addedUser2] = (results as any) as Types.User[];
+
+      await addShoppingListMembers(listId, [addedUser1, addedUser2]);
+      const members = await getShoppingListMembers(listId);
+
+      expect(members.users).toContainEqual(addedUser1);
+      expect(members.users).toContainEqual(addedUser2);
+    });
+
+    it('should delete a shopping list member', async () => {
+      const list = { name: 'testlist' };
+      await addShoppingList(list);
+
+      let results = await sql`SELECT * FROM lists`;
+      const listId = results[0].id;
+
+      const user1 = {
+        nickname: 'test1',
+        email: 'test1@email.com',
+        picture: 'https://picture1.com',
+      };
+      const user2 = {
+        nickname: 'test2',
+        email: 'test2@email.com',
+        picture: 'https://picture2.com',
+      };
+
+      await addUser(user1);
+      await addUser(user2);
+
+      results = await sql`SELECT * FROM users`;
+      const [addedUser1, addedUser2] = (results as any) as Types.User[];
+
+      await addShoppingListMembers(listId, [addedUser1, addedUser2]);
+      await deleteShoppingListMembers(listId, [addedUser1]);
+      const members = await getShoppingListMembers(listId);
+
+      expect(members.users).not.toContainEqual(addedUser1);
+      expect(members.users).toContainEqual(addedUser2);
+    });
   });
 
-  describe('listitems', () => {
-  });
+  describe('listitems', () => {});
 
-  describe('listevents', () => {
-  });
+  describe('listevents', () => {});
 });
