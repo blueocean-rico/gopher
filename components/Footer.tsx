75c05a702fd1ab86c9aed1a8a2aa549f3173fcdd
Din Cohen <dincohen92@gmail.com>
2022-06-29 16:47:11 -0700

    New list form page (#34)
    * new list form
    
    * merge

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,43 +7,52 @@ const Footer: NextPage = () => {
         position: "absolute",
         bottom: 0,
         width: '100%',
-        padding: '50px' }}
+        padding: '25px'
+        }}
       >
         <Grid.Col span={3}>
         <SimpleGrid cols={1}>
-        <Title order={3}>SUPPORT</Title>
-        <Text>Contact Us</Text>
-        <Text>User Guide</Text>
-        <Text>FAQ</Text>
-        <Text>Help</Text>
-        <Text>Examples</Text>
+        <Title order={5}>SUPPORT</Title>
+        <Text>
+          Contact Us<br/>
+          User Guide<br/>
+          FAQ<br/>
+          Help<br/>
+          Examples
+        </Text>
       </SimpleGrid>
         </Grid.Col>
         <Grid.Col span={3}>
         <SimpleGrid cols={1}>
-        <Title order={3}>COMPANY</Title>
-        <Text>About Us</Text>
-        <Text>Media Kit</Text>
-        <Text>Newsletters</Text>
-        <Text>Partnerships</Text>
-        <Text>Blog</Text>
+        <Title order={5}>COMPANY</Title>
+        <Text>
+          About Us<br/>
+          Media Kit<br/>
+          Newsletters<br/>
+          Partnerships<br/>
+          Blog
+        </Text>
       </SimpleGrid>
         </Grid.Col>
         <Grid.Col span={3}>
         <SimpleGrid cols={1}>
-        <Title order={3}>SERVICE</Title>
-        <Text>Compare</Text>
-        <Text>Download</Text>
-        <Text>Feedback</Text>
-        <Text>Bug Report</Text>
+        <Title order={5}>SERVICE</Title>
+        <Text>
+          Compare<br/>
+          Download<br/>
+          Feedback<br/>
+          Bug Report
+        </Text>
       </SimpleGrid>
         </Grid.Col>
         <Grid.Col span={3}>
         <SimpleGrid cols={1}>
-        <Title order={3}>LEGAL</Title>
-        <Text>Terms & Condition</Text>
-        <Text>Terms of Use</Text>
-        <Text>Privacy Policy</Text>
+        <Title order={5}>LEGAL</Title>
+        <Text>
+          Terms & Condition<br/>
+          Terms of Use<br/>
+          Privacy Policy
+        </Text>
       </SimpleGrid>
         </Grid.Col>
     </Grid>
