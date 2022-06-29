d44961619a6d89446186e4ff56cdfaa10499d3de
Sully Clark <clarkjs237@gmail.com>
2022-06-29 13:17:31 -0700

    up to date with main

--- pages/map.tsx
+++ pages/map.tsx
@@ -8,14 +8,11 @@
 
 import React, { useState } from "react";
 import { GoogleMap, LoadScript, MarkerF, useJsApiLoader } from "@react-google-maps/api";
-// import PlaceSearch from "./api/map";
-// import { constants } from "buffer";
+
 
 const containerStyle = {
-  width: '750px',
-  height: '500px'
-  // width: '100%',
-  // height: '100%'
+  width: '100%',
+  height: '100%'
 };
 
 const center = {
@@ -26,11 +23,9 @@ const center = {
 
 const google_maps_api_key: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS ?? '';
 
-
 function HomePage({ stores }) {
   // const [places, setPlaces] = useState(PlaceSearch({ center }));
 
-  console.log(stores);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: google_maps_api_key,
@@ -41,40 +36,38 @@ function HomePage({ stores }) {
   }
 
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={13}
-      options={{
-        zoomControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-        fullscreenControl: false
-      }}
-    >
-      <MarkerF position={center} icon={'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'} />
-      {stores.map((store, index) => (
-        <MarkerF position={store.geometry.location} key={index} />
-      ))}
-    </GoogleMap>
+    <div style={{width: 'auto', height: '455px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <div style={{width: '700px', height: '400px'}}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={13}
+          options={{
+            zoomControl: true,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false
+          }}
+        >
+          <MarkerF position={center} icon={'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'} title='Home'/>
+          {stores.map((store, index) => (
+            <MarkerF position={store.geometry.location} key={index} />
+          ))}
+        </GoogleMap>
+      </div>
+    </div>
+
   );
 }
 
 
 export async function getStaticProps() {
 
-  const center = {
-    lat: 47.6205,
-    lng: -122.349358,
-  };
 
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${center.lat}%2C${center.lng}&radius=1800&type=supermarket&keyword=whole&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS}`
-
-
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${center.lat}%2C${center.lng}&radius=1800&type=supermarket&keyword=grocery&key=${google_maps_api_key}`
 
   const res = await fetch(url, {
     method: 'GET',
-    // mode: 'cors',
     headers: {
       'Content-Type': 'application/json'
     }
