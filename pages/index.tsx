d6ed008541b036d0c846a63a14b45e6b94f75167
dincohen92 <dincohen92@gmail.com>
2022-06-29 14:27:59 -0700

    merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import {useUser} from '@auth0/nextjs-auth0';
 import type { NextPage } from "next";
 import Head from "next/head";
-<<<<<<< HEAD
 import styles from "../styles/Home.module.css";
 import Link from 'next/link'
 import { Title, Button, Grid, Text } from "@mantine/core";
@@ -80,47 +79,5 @@ const Home: NextPage = () => {
     </div>
   )
 };
-=======
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Link from 'next/link'
 
-const Home = () => {
-
-  const {user, error, isLoading} = useUser();
-  console.log(user);
-  // console.log(user);
-  if (isLoading) {
-    return
-    <div>
-      ...loading
-    </div>
-  };
-  if (error) {
-    return <div>
-      {error.message}
-    </div>
-  }
-  if (user) {
-    return (
-    <>
-    <h1>Welcome {user?.nickname}!</h1>
-    <Image src={user?.picture} width={50}
-      height={50} alt='picture'/>
-    <Link href='/api/auth/logout'>
-      <a >Logout</a>
-    </Link>
->>>>>>> set up auth0
-
-    </>
-    )
-  }
-  return(
-  <>
-  <Link href='/api/auth/login'>
-   <a >Login</a>
- </Link>
- </>
-  )
-};
 export default Home;
