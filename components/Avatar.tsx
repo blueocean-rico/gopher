fa21991027c8f1221cf8ef9f1a0c26908516d957
Angela Mao <99843023+Angelamaomao@users.noreply.github.com>
2022-06-30 11:27:51 -0700

    adding avatar (#41)

---
+++ components/Avatar.tsx
@@ -0,0 +1,10 @@
+import { Avatar as MantineAvatar } from '@mantine/core';
+import type { User } from '@/types/index';
+
+const Avatar = ({ user, size }: {user: User; size: 'xs' | 'sm' | 'md';}) => {
+  return (
+    <MantineAvatar src={user.picture} alt={user.nickname} radius="md" size={size}>
+      {user.nickname.charAt(0)}
+   </MantineAvatar>
+  );
+};
