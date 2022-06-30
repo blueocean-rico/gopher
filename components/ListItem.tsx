42dc8719580bd21ec5dfea7d6c8dd451ebcebe1d
zman811 <sunburnzman@gmail.com>
2022-06-30 11:27:53 -0600

    Basic changes

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,16 +1,21 @@
-import { Box, Text, Paper, CloseButton, Group } from '@mantine/core';
+import { Box, Text, Paper, CloseButton, Group, Button } from '@mantine/core';
+import { Pencil } from 'tabler-icons-react';
 
-export default function ListItem({ item, price, members }) {
+export default function ListItem({ item, editable, small }) {
   const handleDelete = (event) => {};
+  const handleEdit = (event) => {}
 
-  return (
-    <Paper shadow="xs" p="xs" withBorder>
-      <Group position="apart">
-        <Box>{item}</Box>
-        <Box>${price}</Box>
-        <Box>{members.join(', ')}</Box>
-        <CloseButton onClick={handleDelete} />
-      </Group>
-    </Paper>
-  );
+  if(editable) {
+    return (
+      <Paper shadow="xs" p="xs" withBorder>
+        <Group position="apart">
+          <Box>{item.name}</Box>
+          <Box>${item.price}</Box>
+          <Box>{item.members.join(', ')}</Box>
+          <Button variant="subtle" color="dark" onClick={handleDelete}>X</Button>
+          <Button variant="subtle" color="dark" onClick={handleEdit}><Pencil /></Button>
+        </Group>
+      </Paper>
+    );
+  }
 }
