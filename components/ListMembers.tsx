f5235b34b3fd92bff6a55e94b337477e779f221c
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-06-30 16:38:38 -0700

    Setting up gopher image.

--- components/ListMembers.tsx
+++ components/ListMembers.tsx
@@ -1,13 +1,25 @@
-import { Box, Paper, Stack, Title } from '@mantine/core';
+import { Box, Paper, Stack, Title } from "@mantine/core";
+// import gopherIcon from "@/public/gopherIcon"
 
-export default function ListMembers({ members }) {
+export default function ListMembers({ listId: number, users. gopher }) {
   return (
     <Paper>
       <Title order={2}>Members</Title>
       <Stack>
-        {members.map((member) => (
-          <Box key={member}>{member}</Box>
+        {users.map((user) => (
+          // <Image
+          //   src={gopherIcon}
+          //   width={10}
+          //   height={10}
+          //   alt="google profile image"
+          //   withPlaceholder
+          // />
+          <User key={user.id} user={user} />
+          <Button variant="default">x</Button>
         ))}
+        <Button variant="default">Add User</Button>
+        <Button variant="default">Randomize Gopher</Button>
+        <Button variant="default">Claim Gopher</Button>
       </Stack>
     </Paper>
   );
