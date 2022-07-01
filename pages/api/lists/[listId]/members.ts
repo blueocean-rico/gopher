0ab1fa12aff47505e24700cab378af3af718fcd4
Shannon Largman <Shannon.largman@gmail.com>
2022-06-30 21:35:36 -0700

    Add ListMembers (#53)
    * Added getGopher to lists[listid] to pass to listmembers.
    
    * Setting up gopher image.
    
    * Added listMembers component, and added put to members api, and added gopher icon.
    
    Co-authored-by: Ryan Snow <74696050+r-snow@users.noreply.github.com>

--- pages/api/lists/[listId]/members.ts
+++ pages/api/lists/[listId]/members.ts
@@ -1,12 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { User, Member } from '@/types/index';
-import { addListMembers, deleteListMembers } from '@/server/lists/index';
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { User, Member } from "@/types/index";
+import {
+  addListMembers,
+  deleteListMembers,
+  setGopher,
+} from "@/server/lists/index";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const listId = Number(req.query.listId);
     const { users } = <{ users: User[] }>req.body;
     try {
@@ -16,11 +20,21 @@ export default async function handler(
       console.log(error);
       res.status(500).send(undefined);
     }
-  } else if (req.method === 'DELETE') {
+  } else if (req.method === "DELETE") {
     const listId = Number(req.query.listId);
     const { users } = <{ listId: number; users: Member[] }>req.body;
     try {
       await deleteListMembers(listId, users);
+      res.status(204).send(undefined);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(undefined);
+    }
+  } else if (req.method === "PUT") {
+    const listId = Number(req.query.listId);
+    const { user } = <{ listId: number; user: Member }>req.body;
+    try {
+      await setGopher(listId, user);
       res.status(204).send(undefined);
     } catch (error) {
       console.log(error);
