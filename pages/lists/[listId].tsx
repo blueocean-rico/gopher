8317da001870ba015e962b8226af218dabaeb45c
Shannon Largman <shannon.largman@gmail.com>
2022-06-30 23:12:10 -0700

    add basic list page layout

--- pages/lists/[listId].tsx
+++ pages/lists/[listId].tsx
@@ -1,33 +1,60 @@
-import type { NextPage } from "next";
-import type { GetServerSideProps } from "next";
-import type { ListItem, User, ListItemEvent } from "@/types/index";
-import {} from "@/components/index";
+import { Button, Title, Group, Stack } from '@mantine/core';
+import { Clock } from 'tabler-icons-react';
+import type { NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
+import type { ListItem, Member, User, ListItemEvent } from '@/types/index';
+import { List, ListItemEdit, ListMembers } from '@/components/index';
+import { getUsers } from '@/server/users/index';
 import {
   getListItems,
   getListMembers,
   getListItemEvents,
-  getGopher,
-} from "@/server/lists/index";
+} from '@/server/lists/index';
 
 interface Props {
+  listId: number;
   items: ListItem[];
-  members: User[];
+  members: Member[];
   events: ListItemEvent[];
+  users: User[];
 }
 
-const ListPage: NextPage<Props> = ({ items, members, events }) => {
+const ListPage: NextPage<Props> = ({
+  listId,
+  items,
+  members,
+  events,
+  users,
+}) => {
   console.log('items', items, 'members', members, 'events', events);
-  return <></>;
+  return (
+    <Group>
+      <Stack>
+        {/*TODO: set up fetching of actual list from db by listId (need to change lists.dal*/}
+        <Title order={1}>List Name</Title> <Clock />
+        <ListItemEdit users={users} />
+        <List listId={listId} items={items} users={users} />
+      </Stack>
+      <Stack>
+        <Title order={2}>Members</Title>
+        <ListMembers listId={listId} members={members} users={users} />
+        {/*TODO: link to checkout page*/}
+        <Button>Checkout</Button>
+      </Stack>
+    </Group>
+  );
 };
 
 export default ListPage;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const listId = Number(context.query.listId);
-  const items = await getListItems(listId);
-  const members = await getListMembers(listId);
-  const events = await getListItemEvents([listId]);
-  const gopher = await getGopher(listId);
+  const [users, items, members, events] = await Promise.all([
+    getUsers(),
+    getListItems(listId),
+    getListMembers(listId),
+    getListItemEvents([listId]),
+  ]);
 
-  return { props: { items, members, events, gopher } };
+  return { props: { listId, items, members, events, users } };
 };
