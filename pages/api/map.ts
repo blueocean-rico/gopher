cd110f532b11956c3fb57ed20243c3703095b30e
Sully Clark <clarkjs237@gmail.com>
2022-07-01 09:53:28 -0700

    Map v1 (#52)
    * first commit on maps branch
    
    * successfully display map
    
    * Map is up. Need to figure "headers" error on api/map.
    
    * marker staying on point
    
    * got maps added using locations and marker
    
    * mapping new markers to google map
    
    * up to date with main
    
    * map better
    
    * better functioning map, need to do directions
    
    * cleaned search button, starting directions
    
    * added recenter functionality
    
    * routes more or less working, can't remove them yet though
    
    * basic search feature + re rendering of markers done
    
    * basic search feature + routing working
    
    * Notifications + Notification draft (#50)
    
    * initial notifications draft
    
    * add initial notification draft
    
    * Add ListMembers (#53)
    
    * Added getGopher to lists[listid] to pass to listmembers.
    
    * Setting up gopher image.
    
    * Added listMembers component, and added put to members api, and added gopher icon.
    
    Co-authored-by: Ryan Snow <74696050+r-snow@users.noreply.github.com>
    
    * fix module resolution (#54)
    
    We were having issues with imports due to use of `export default`. This exports the function as an unnamed export which interferes with named importing from the index (e.g. `import { Component } from '@/components/index'`).
    
    See https://stackoverflow.com/questions/37564906/what-are-all-the-index-ts-used-for for more info.
    
    * Add basic layout to lists and list pages (#56)
    
    * add basic list page layout
    
    * tweak list page components
    
    * tweak ListItemEdit
    
    * add basic lists page layout
    
    * remove unneeded households page
    
    The households page was crashing the build.
    
    * fix test to reflect new getListMembers return
    
    getListMembers was changed to return an array of list members rather
    than an object with a users propery corresponding to that array for
    better consistency with the other get functions.
    
    * removed node modules, package-lock.json, did a pull from main, and re-installed dependencies
    
    * added postgres package to package.json
    
    Co-authored-by: Ryan Snow <74696050+r-snow@users.noreply.github.com>
    Co-authored-by: Shannon Largman <Shannon.largman@gmail.com>

---
+++ pages/api/map.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // These are the lat/lng values of the user determined center
+  const { searchValue, lat, lng } = req.query;
+
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=store&keyword=${searchValue}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS}`
+
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await response.json();
+
+  const stores = data.results;
+
+  res.status(200).json({ stores });
+}
